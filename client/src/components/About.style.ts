797bef1451e3ef45ada65991312e44e555daea67
Kim <kimly.jet@gmail.com>
2022-11-05 20:56:52 -0700

    Feat: Add About section

--- client/src/components/About.style.ts
+++ client/src/components/About.style.ts
@@ -1,22 +1,51 @@
 import styled from "styled-components";
+import aboutBackground from "../assets/about-background.svg";
 
 const AboutSectionContainer = styled.section`
   position: relative;
-  height: 100vh;
+  height: 70vh;
   display: flex;
   overflow: hidden;
+  background-image: url(${aboutBackground});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
-const AboutImageContainer = styled.div`
-  width: 50%;
-  background: ${(props) => props.theme.colors.background_dark};
+const AboutImageContainer = styled.article`
+  width: 46%;
+  margin: auto;
 `;
 
-const AboutInfoContainer = styled.div`
-  width: 50%;
-  background: ${(props) => props.theme.colors.background_light};
+const AboutImage = styled.img`
+  width: 70%;
+  border-radius: 1rem;
+  margin-left: 4rem;
+`;
+
+const AboutInfoContainer = styled.article`
+  width: 54%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-export { AboutSectionContainer, AboutImageContainer, AboutInfoContainer };
+const AboutInfoTitle = styled.h2`
+  width: 70%;
+  font-size: ${(props) => props.theme.fontSize.h2};
+`;
+
+const AboutInfoText = styled.p`
+  width: 70%;
+  font-size: ${(props) => props.theme.fontSize.text};
+  line-height: 1.5;
+`;
+
+export {
+  AboutSectionContainer,
+  AboutImageContainer,
+  AboutImage,
+  AboutInfoContainer,
+  AboutInfoTitle,
+  AboutInfoText,
+};
--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,14 +1,28 @@
 import {
   AboutSectionContainer,
   AboutImageContainer,
+  AboutImage,
   AboutInfoContainer,
+  AboutInfoTitle,
+  AboutInfoText,
 } from "./About.style";
+import _aboutData from "../data/about.json";
+import { _about } from "../globals";
+import space from "../assets/space.jpg";
+
+const aboutData = _aboutData as _about[];
 
 const About = () => {
   return (
     <AboutSectionContainer>
-      <AboutImageContainer>Image</AboutImageContainer>
-      <AboutInfoContainer>Text</AboutInfoContainer>
+      <AboutImageContainer>
+        <AboutImage src={space} />
+      </AboutImageContainer>
+      <AboutInfoContainer>
+        <AboutInfoTitle>{aboutData[0].title}</AboutInfoTitle>
+        <AboutInfoText>{aboutData[0].text1}</AboutInfoText>
+        <AboutInfoText>{aboutData[0].text2}</AboutInfoText>
+      </AboutInfoContainer>
     </AboutSectionContainer>
   );
 };
