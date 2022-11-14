94bdb118d20e233c9caa600b8fc7ccf7254317eb
Kim <kimly.jet@gmail.com>
2022-11-13 23:16:55 -0800

    Chore: Add Solutions component to Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Hero from "../components/Hero";
+import Solutions from "../components/Solutions";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Solutions />
       <Contact />
     </>
   );
