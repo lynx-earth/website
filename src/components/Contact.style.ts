0418dad34b13616a8c9218a0c2e7f21ea0898636
Kim <kimly.jet@gmail.com>
2022-11-15 20:10:32 -0800

    Style: Add mobile responsiveness to Contact section

--- src/components/Contact.style.ts
+++ src/components/Contact.style.ts
@@ -2,30 +2,49 @@ import styled from "styled-components";
 
 const ContactSectionContainer = styled.footer`
   position: relative;
-  height: 75vh;
+  height: 100vh;
   display: flex;
+  flex-direction: column;
   overflow: hidden;
   justify-content: center;
+  align-items: center;
+  margin: 2rem 0;
+
+  @media (min-width: 768px) {
+    margin: 0;
+    height: 75vh;
+    flex-direction: row;
+  }
 `;
 
 const GoogleMapContainer = styled.article`
-  width: 50%;
+  flex-basis: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 2rem;
 
   iframe {
-    width: 80%;
-    height: 80%;
-    border-radius: 2rem;
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+    @media (min-width: 768px) {
+      width: 80%;
+      height: 50vh;
+    }
+  }
+
+  @media (min-width: 768px) {
+    margin: 0;
   }
 `;
 
 const GoogleMap = styled.div``;
 
 const ContactInfoContainer = styled.article`
-  width: 40%;
+  flex-basis: 40%;
   display: flex;
   flex-direction: column;
   justify-content: center;
